4b99fffd4bbb68de699b10c5c3c17a752eee7c05
Jean-André Santoni <jean.andre.santoni@gmail.com>
2024-07-28 14:01:09 +0200

    Banks

---
+++ bank2.h
@@ -0,0 +1,27 @@
+extern const unsigned char	turnip_spr3_left_psgcompr[70];
+#define				turnip_spr3_left_psgcompr_size 70
+#define				turnip_spr3_left_psgcompr_bank 2
+extern const unsigned char	turnip_spr3_right_psgcompr[70];
+#define				turnip_spr3_right_psgcompr_size 70
+#define				turnip_spr3_right_psgcompr_bank 2
+extern const unsigned char	turnip_spr0_right_psgcompr[69];
+#define				turnip_spr0_right_psgcompr_size 69
+#define				turnip_spr0_right_psgcompr_bank 2
+extern const unsigned char	turnip_spr0_left_psgcompr[69];
+#define				turnip_spr0_left_psgcompr_size 69
+#define				turnip_spr0_left_psgcompr_bank 2
+extern const unsigned char	turnip_spr2_right_psgcompr[68];
+#define				turnip_spr2_right_psgcompr_size 68
+#define				turnip_spr2_right_psgcompr_bank 2
+extern const unsigned char	turnip_spr2_left_psgcompr[68];
+#define				turnip_spr2_left_psgcompr_size 68
+#define				turnip_spr2_left_psgcompr_bank 2
+extern const unsigned char	turnip_spr1_left_psgcompr[67];
+#define				turnip_spr1_left_psgcompr_size 67
+#define				turnip_spr1_left_psgcompr_bank 2
+extern const unsigned char	turnip_spr1_right_psgcompr[67];
+#define				turnip_spr1_right_psgcompr_size 67
+#define				turnip_spr1_right_psgcompr_bank 2
+extern const unsigned char	turnip_spr_pal_bin[8];
+#define				turnip_spr_pal_bin_size 8
+#define				turnip_spr_pal_bin_bank 2
